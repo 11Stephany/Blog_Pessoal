a20740672a3d3344f56dbeb1a2109b29e2f77c70
stephany souza <stephanysr@hotmail.com>
2022-10-04 17:33:22 -0300

    Ajuste do TemaRepository no parametro

--- blogpessoal/src/main/java/com/example/demo/blogpessoal/repository/TemaRepository.java
+++ blogpessoal/src/main/java/com/example/demo/blogpessoal/repository/TemaRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.blogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.blogpessoal.model.Tema;
@@ -10,6 +11,6 @@ import com.example.demo.blogpessoal.model.Tema;
 @Repository
 public interface TemaRepository extends JpaRepository <Tema,Long> {
 	
-	public List <Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+	public List <Tema> findAllByDescricaoContainingIgnoreCase(@Param ("descricao")String descricao);
 	
 }
