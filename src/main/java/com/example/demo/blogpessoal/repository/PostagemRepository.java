05a83dc65595513c9110b146c9db6387c5fb6fde
stephany souza <stephanysr@hotmail.com>
2022-10-20 19:40:37 -0300

    Preparando api para deploy

--- src/main/java/com/example/demo/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/example/demo/blogpessoal/repository/PostagemRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.blogpessoal.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
